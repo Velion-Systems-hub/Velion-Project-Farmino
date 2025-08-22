fe4fffa6f2167f6decec05d2b35714ed58df2f1c
psalmuel <samuelonyedikachi450@gmail.com>
2025-08-22 01:48:11 +0100

    responsiveness

--- responsive.js
+++ responsive.js
@@ -1,6 +1,6 @@
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const mobileNav = document.querySelector('.mobile-nav');
 
-mobileMenuBtn.addEventListener('click', () => {
-  mobileNav.classList.toggle('active');
+mobileMenuBtn.addEventListener('click', () =>{
+    mobileNav.classList.toggle('active');
 });
